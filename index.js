3b90ef9c2500d75daf46937505bdf47cc0bd3f2f
Adam Davis <adam@admataz.com>
2016-02-25 23:24:13 +0000

    fixes some race conditions with async

---
+++ index.js
@@ -0,0 +1,28 @@
+var buildList = require('./lib/buildList');
+var buildPages = require('./lib/buildPages');
+var compilePage = require('./lib/compilePage');
+var indexPages = require('./lib/indexPages');
+var loadDataObjectList = require('./lib/loadDataObjectList');
+var loadDocument = require('./lib/loadDocument');
+var loadDocumentList = require('./lib/loadDocumentList');
+var loadJsonElements = require('./lib/loadJsonElements');
+var loadJsonVars = require('./lib/loadJsonVars');
+var loadPageIndexList = require('./lib/loadPageIndexList');
+var loadTemplate = require('./lib/loadTemplate');
+var writeHtmlPage = require('./lib/writeHtmlPage');
+
+
+module.exports = {
+  buildList,
+  buildPages,
+  compilePage,
+  indexPages,
+  loadDataObjectList,
+  loadDocument,
+  loadDocumentList,
+  loadJsonElements,
+  loadJsonVars,
+  loadPageIndexList,
+  loadTemplate,
+  writeHtmlPage
+};
